f68ed3ec11ed21115491dd6ddbc5410ac66aab5f
xiatian <xiatian1119@gmail.com>
2019-08-19 13:46:08 +0800

    add tags in Articles

--- src/main/scala/ruc/irm/extractor/keyword/Articles.scala
+++ src/main/scala/ruc/irm/extractor/keyword/Articles.scala
@@ -20,26 +20,29 @@ object Articles {
   /**
     * 文章的平均字符数量： 2629.101
     */
-  lazy val averageContentLength = (doc \\ "content").map(_.text.length).foldLeft(0)(_ + _) /articleCount.toDouble
+  lazy val averageContentLength = (doc \\ "content").map(_.text.length).foldLeft(0)(_ + _) / articleCount.toDouble
 
   /**
     * 文章的平均词语数量: 1631.814
     */
-  lazy val averageContentWords = (doc \\ "content").map(e=>Segment.segment(e.text).length).foldLeft(0)(_ + _) /articleCount.toDouble
+  lazy val averageContentWords = (doc \\ "content").map(e => Segment.segment(e.text).length).foldLeft(0)(_ + _) / articleCount.toDouble
 
   private val acceptPOS = (pos: String) => pos.take(1) == "n" || pos.take(3) == "adj" || pos.take(1) == "v"
 
   /**
     * 文章的平均词语数量： 过滤后只保留名词，动词和形容词: 757.284
     */
-  lazy val filteredAverageContentWords = (doc \\ "content").map(a=>Segment.segment(a.text).filter(b=>acceptPOS(b._2)).length).foldLeft(0)(_ + _) /articleCount.toDouble
+  lazy val filteredAverageContentWords = (doc \\ "content").map(a => Segment.segment(a.text).filter(b => acceptPOS(b._2)).length).foldLeft(0)(_ + _) / articleCount.toDouble
 
   /**
     * 获取第id篇文章内容
     */
-  lazy val getArticle = (id:Int) => Map("url"-> (articles(id) \ "url").text,
-                      "title"->(articles(id) \ "title").text,
-                      "tags"->(articles(id) \ "tags").text,
-                      "content"->(articles(id) \ "content").text
-                      )
+  lazy val getArticle = (id: Int) => Map("url" -> (articles(id) \ "url").text,
+    "title" -> (articles(id) \ "title").text,
+    "tags" -> (articles(id) \ "tags").text,
+    "content" -> (articles(id) \ "content").text
+  )
+
+  def tags(articleId: Int): String = (articles(articleId) \ "tags").text
+
 }
